46e59470c5bde45b49d3b8f96ca537fed110ae2a
veryhappywing <godlikewing@gmail.com>
2018-06-21 17:12:35 +0800

    initial version

--- demo/src/main/java/com/pax/android/demoapp/DownloadParamService.java
+++ demo/src/main/java/com/pax/android/demoapp/DownloadParamService.java
@@ -1,17 +1,3 @@
-/*
- * *
- *     * ********************************************************************************
- *     * COPYRIGHT
- *     *               PAX TECHNOLOGY, Inc. PROPRIETARY INFORMATION
- *     *   This software is supplied under the terms of a license agreement or
- *     *   nondisclosure agreement with PAX  Technology, Inc. and may not be copied
- *     *   or disclosed except in accordance with the terms in that agreement.
- *     *
- *     *      Copyright (C) 2017 PAX Technology, Inc. All rights reserved.
- *     * ********************************************************************************
- *
- */
-
 package com.pax.android.demoapp;
 
 import android.app.Service;
