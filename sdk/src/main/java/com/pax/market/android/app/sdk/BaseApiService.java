d1a1cc05465850f3d62dd68a8ef6c5df6f2bdd52
Sylar <fanjun@paxsz.com>
2020-09-23 09:58:35 +0800

    PAXSTORE-19666 Update proxy logs

--- sdk/src/main/java/com/pax/market/android/app/sdk/BaseApiService.java
+++ sdk/src/main/java/com/pax/market/android/app/sdk/BaseApiService.java
@@ -77,9 +77,10 @@ public class BaseApiService implements ProxyDelegate {
                 try {
                     StoreProxyInfo proxyInfo = IApiUrlService.Stub.asInterface(service).getStoreProxyInfo();
                     if(proxyInfo != null) {
-                        logger.info(">>> Init proxy from PAXSTORE : proxy[@{}/{}:{}], has proxy authenticator={}",
+                        logger.info(">>> Init proxy from PAXSTORE : proxy[@{}/{}:{}], proxy authentication={}",
                                 proxyInfo.getType() == 1 ? "HTTP" : proxyInfo.getType() == 2 ? "SOCKS" : "DIRECT",
-                                proxyInfo.getHost(), proxyInfo.getPort(), proxyInfo.getAuthorization() != null);
+                                proxyInfo.getHost(), proxyInfo.getPort(),
+                                proxyInfo.getAuthorization() != null ? "Baisc" : proxyInfo.getUsername() != null ? "Password" : "NULl");
                     } else {
                         logger.warn(">>> Init proxy from PASXTORE : [NULL]");
                     }
