86eae0ae70d82a1f724fd9101275c76974bef73e
zhangcy <zhangcy@szzolon.com>
2026-01-08 16:38:42 +0800

    request permission for notification for android 13+

--- demo/src/main/java/com/pax/android/demoapp/view/LauncherActivity.java
+++ demo/src/main/java/com/pax/android/demoapp/view/LauncherActivity.java
@@ -1,5 +1,6 @@
 package com.pax.android.demoapp.view;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
@@ -11,12 +12,15 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -121,7 +125,39 @@ public class LauncherActivity extends FragmentActivity implements APIFragment.On
 
             }
         });
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            checkNotificationPermission();
+        }, 1500);
+    }
 
+    private static final int NOTIFICATION_PERMISSION_REQUEST_CODE = 101;
+
+    private void checkNotificationPermission() {
+        // only above Android 13 (API 33) need to request notification permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // request notification permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS},
+                        NOTIFICATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == NOTIFICATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Notification permission permitted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Notification permission is denied. You will not receive any reminders.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 
